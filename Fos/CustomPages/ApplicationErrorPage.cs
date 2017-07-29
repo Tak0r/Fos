505517e22e3469af46137a67213a6e74766d80a4
Frank Gehann <gehannf@gmail.com>
2017-07-29 17:29:41 +0200

    make the ApplicationErrorPage string better readable

--- Fos/CustomPages/ApplicationErrorPage.cs
+++ Fos/CustomPages/ApplicationErrorPage.cs
@@ -4,13 +4,23 @@ namespace Fos.CustomPages
 {
 	internal class ApplicationErrorPage : ICustomPage
 	{
-		private static string PageFormat = "<html><head><title>Application Error</title></head><body><h1>Application Error</h1><p>Your application could not process the request and threw the following exception:</p><p>{0}</p></body></html>";
+		private static string PageFormat = @"
+            <html>
+                <head>
+                    <title>Application Error</title>
+                </head>
+                <body>
+                    <h1>Application Error</h1>
+                    <p>Your application could not process the request and threw the following exception:</p>
+                    <p>{0}</p>
+                </body>
+            </html>";
 
 		public string Contents { get; private set; }
 
 		public ApplicationErrorPage(Exception e)
 		{
-			Contents = string.Format(PageFormat, e.ToString().Replace(System.Environment.NewLine, "<br />"));
+			Contents = string.Format(PageFormat, e.ToString().Replace(Environment.NewLine, "<br />"));
 		}
 	}
 }
